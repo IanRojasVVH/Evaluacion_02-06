ad2476b1bd4bb808a6d4a137fc61a1c32041ebbc
IanRojasVVH <ianrojas@gmail.com>
2025-06-09 21:53:30 -0400

    ok

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Esta función se activa cuando hacemos clic en una imagen
 function mostrarTexto(imagen) {
+    // Busca el siguiente elemento después de la imagen (en este caso, el <div> que tiene el texto)
     const texto = imagen.nextElementSibling;
+    // Activa o desactiva la clase "mostrar" para que el texto se vea o se oculte
     texto.classList.toggle("mostrar");
 }
